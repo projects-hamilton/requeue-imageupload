f879dd99b7fae188d9278a20e279cbe83aa61e32
Rubiyadav19 <rubi_in@hamiltonkw.com>
2022-09-30 10:42:43 +0530

    upadte

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const express = require('express')
 const app = express()
 require('dotenv').config();
 const db = process.env.DB_URI
-
+const cors = require("cors");
+app.use(cors());
 
 app.use(express.json())
 const port = process.env.PORT || 3000
@@ -32,11 +33,7 @@ app.use('/survey',require('./routes/survey'))
 app.use("/answer",require("./routes/answer"))
 app.use('/user',require('./routes/user'))
 
-const cors = require("cors");
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PATCH", "DELETE"],
-}));
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
